283872679710e53887d6bfdd70b40941941fc734
king-open <kingwang.0624@gmail.com>
2022-07-15 10:38:59 +0800

    让topnav 是响应式的

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,6 +5,7 @@
         <li>菜单1</li>
         <li>菜单2</li>
       </ul>
+      <span class="toggleAside"></span>
     </div>
 </template>
 
@@ -28,6 +29,8 @@ import {inject,Ref} from 'vue'
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
   > .logo{
     max-width: 6em;
     margin-right: auto;
@@ -40,5 +43,12 @@ import {inject,Ref} from 'vue'
       margin: 0 1em;
     }
   }
+  > .toggleAside{}
+  @media (max-width:500px) {
+    >.menu{display: flex;}
+    >.logo{margin: 0 auto;}
+    
+  }
+  
 }
 </style>

2b7a1e73afd4f9242bfaaa0aebde50bc7b3ef757
king-open <kingwang.0624@gmail.com>
2022-07-15 23:41:36 +0800

    CSS 最小影响原则

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="toggle" :class="{ checked: value }">
+  <button class="to-switch" @click="toggle" :class="{ 'to-checked': value }">
     <span></span>
   </button>
 </template>
@@ -20,27 +20,21 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {height: $h;width: $h * 2;border: none;background: #bfbfbf;border-radius: $h/2;position: relative;
+.to-Switch {height: $h;width: $h * 2;border: none;background: #bfbfbf;border-radius: $h/2;position: relative;
   > span {position: absolute;top: 2px;left: 2px; height: $h2;width: $h2;background: white;border-radius: $h2 / 2;transition: all 250ms;
   }
-  &.checked {background: #1890ff;
+  &.to-checked {background: #1890ff;
     > span {left: calc(100% - #{$h2} - 2px);}
   }
   &:focus {outline: none;}
   &:active {
   > span {width: $h2 + 4px;}
   }
-&.checked:active {
+&.to-checked:active {
   > span {width: $h2 + 4px;margin-left: -4px;}
 }
 }
-
-
- 
-
-
-
 </style>

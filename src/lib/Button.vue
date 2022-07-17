e6df103a37886769948e652935648738c432c8e0
king-open <kingwang.0624@gmail.com>
2022-07-17 13:33:30 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -15,15 +15,20 @@ export default {
     },
     size:{
       type:String,
-      default:'button'
+      default:'normal'
+    },
+    level:{
+      type:String,
+      default:'normal',
     },
   },
   setup(props){
-    const {theme,size} = props;
+    const {theme,size,level} = props;
     const classes = computed(()=>{
       return {
         [`to-theme-${theme}`]:theme,
         [`to-size-${size}`]:size,
+        [`to-level-${level}`]:level,
       };
     });
     return {classes}
@@ -36,6 +41,7 @@ $border-color:#d9d9d9;
 $color:#333;
 $blue:#40a9ff;
 $radius:4px;
+$red:red;
 .to-button{
   box-sizing: border-box;
   height: $h;
@@ -49,6 +55,7 @@ $radius:4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black,0.95);
+  transition: background 250ms;
   &+&{
     margin-left: 8px;
   }
@@ -87,6 +94,48 @@ $radius:4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.to-theme-button{
+    &.to-level-main{
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,&:focus{
+        background: darken($blue,10%);
+        border-color: darken($blue,10%);
+      }
+    }
+    &.to-level-danger{
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,&:focus{
+        background: darken($blue,10%);
+        border-color: darken($blue,10%);
+      }
+    }
+  }
+  &.to-theme-link{
+    &.to-level-danger{
+      color: $red;
+      &:hover,&:focus{
+        color: darken($red,10%);
+      }
+    }
+  }
+  &.to-theme-text{
+    &.to-level-main{
+      color: $blue;
+      &:hover,&:focus{
+        color: darken($blue,10%);
+      }
+    }
+    &.to-level-danger{
+      color: $red;
+      &:hover,&:focus{
+        color: darken($red,10%);
+      }
+    }
   }
 }
 </style>
